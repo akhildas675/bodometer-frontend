8eca26f79c65259b62791041c4e31258357049d1
akhildas675 <akhildas675@gmail.com>
2026-01-20 21:36:04 +0530

    useFetch added on user side to fetch users

--- src/components/admin/user.management/admin-users.management.tsx
+++ src/components/admin/user.management/admin-users.management.tsx
@@ -1,36 +1,23 @@
-import { useEffect, useState, useCallback } from "react";
 import SidebarLayout from "../../ui/app.sidebar/sidebar.layout";
 import { useAuthStore } from "../../../stores/auth.store";
-
 import { userColumns } from "./admin-users.columns";
 import { useUserActions } from "./admin-users.actions";
 import adminServices from "../../../services/admin/admin.services";
 import type { AdminGetUsersResponse } from "../../../interface/admin.interface";
 import DataTable from "../../ui/table/data.table";
+import { useFetch } from "../../../hooks/userFetch";
 
 const AdminUsersManagement = () => {
   const user = useAuthStore((state) => state.user);
-  const [users, setUsers] = useState<AdminGetUsersResponse[]>([]);
-  const [loading, setLoading] = useState(true);
 
-
-  const fetchUsers = useCallback(async () => {
-    try {
-      setLoading(true);
-      const res = await adminServices.getUsers();
-      console.log("Users from backend", res.data);
-      setUsers(res.data);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
-
-
-  const actions = useUserActions(fetchUsers);
-
-  useEffect(() => {
-    fetchUsers();
-  }, [fetchUsers]);
+  const {
+    data: users,
+    loading,
+    refetch,
+  } = useFetch<AdminGetUsersResponse[]>(() =>
+    adminServices.getUsers().then((res) => res.data),
+  );
+  const actions = useUserActions(refetch);
 
   if (!user) {
     return <div className="text-white p-6">Loading...</div>;
